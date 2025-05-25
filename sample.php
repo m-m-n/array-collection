d0c291441b7a4a86f42304979e238bb2ca2cf465
sakura <trimdevel@gmail.com>
2025-05-25 21:54:26 +0900

    コメント追加

--- sample.php
+++ sample.php
@@ -10,11 +10,15 @@ var_dump(phpversion());
 $a = ["foo" => "bar"];
 $b = new HashMap($a);
 
+// PHP 7系はWarning出ないけどPHP 8系はWarning出る
 var_dump($a["bar"]);
+// PHP 7系もPHP 8系もWarning出ない
 var_dump($b["bar"]);
 
 $c = [1, 2, 3];
 $d = new Vector($c);
 
+// PHP 7系はWarning出ないけどPHP 8系はWarning出る
 var_dump($c[3]);
+// PHP 7系もPHP 8系もWarning出ない
 var_dump($d[3]);
